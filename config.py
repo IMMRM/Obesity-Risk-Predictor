0b2dbdddbb4d2a01f8bf124b594bf49532cd1b32
Immrm <mrmirdha@outlook.com>
2024-02-10 15:20:32 +0530

    Pre-processing done

---
+++ config.py
@@ -0,0 +1,15 @@
+#This is the configuration file
+
+
+#train data
+raw_train_data="../data/train.csv"
+
+#test data
+raw_test_data="../data/test.csv"
+
+#models
+models="../models/"
+
+#input
+input="../inputs/"
+
